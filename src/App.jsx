eeb5a0cb7aeb48e28b643fa3496de8fe0468854f
howardyangyixuan <howardyangyixuan@gmail.com>
2022-05-13 01:14:46 +0800

    connect：输入一个组件，返回一个包装好的组件（为原组件提供dispatch）。这样直接使用包装好的组件即可。 但当前方案由于使用了App里的setState，所以app里的所有函数在渲染时都会重新执行。实际希望所有用到数据的地方，只有数据变化了才重新执行。用useMemo包裹是一个缓兵之计，但编码效率太低并且容易产生bug。

--- src/App.jsx
+++ src/App.jsx
@@ -29,21 +29,25 @@ export const App = () => {
   )
 }
 const Child1 = () => <section>Child1<User/></section>
-const Child2 = () => <section>Child2<Wrapper/></section>
+const Child2 = () => <section>Child2<UserModifier/></section>
 const Child3 = () => <section>Child3</section>
 const User = () => {
   const {appState} = useContext(appContext)
   return <div>User:{appState.user.name}</div>
 
 }
-const Wrapper = () => {
-  const {appState, setAppState} = useContext(appContext)
-  const dispatch = (action) => {
-    setAppState(reducer(appState, action))
+
+const connect = (Component) => {
+  //返回一个对Component进行了包装的新函数组件
+  return () => {
+    const {appState, setAppState} = useContext(appContext)
+    const dispatch = (action) => {
+      setAppState(reducer(appState, action))
+    }
+    return <Component dispatch={dispatch} appState={appState}/>
   }
-  return <UserModifier dispatch={dispatch} appState={appState}/>
 }
-const UserModifier = (props) => {
+const UserModifier = connect((props) => {
   const {dispatch, appState} = props
   const onChange = (e) => {
     dispatch({type: "updateUser", payload: {name: e.target.value}})
@@ -52,4 +56,4 @@ const UserModifier = (props) => {
     <input value={appState.user.name}
            onChange={onChange}/>
   </div>
-}
+})
