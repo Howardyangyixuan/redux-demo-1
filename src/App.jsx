4a6e3e3a03afb8384bacf2cc48f75ac9974964bd
howardyangyixuan <howardyangyixuan@gmail.com>
2022-05-13 10:27:20 +0800

    更通用的解决方式是将数据及操作单独存储为store变量，state/setState读写数据，并提供订阅发布接口，在setState数据变动时，调用组件传入的函数（发布）。所有依赖数据的组件都需要订阅，因此在connect中提供数据给组建时，可以同时进行订阅（由于只需订阅一次，因此使用useEffect）。

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import React, {useState, useContext} from "react"
+import React, {useState, useContext, useEffect} from "react"
 
 const appContext = React.createContext(null)
 const store = {
   state: {
     user: {name: "howard", age: 23}
   },
+  listeners: [],
+  subscribe(fn) {
+    store.listeners.push(fn)
+    return () => {
+      const idx = store.listeners.indexOf(fn)
+      this.listeners.splice(idx, 1)
+    }
+  },
+  update() {
+    store.listeners.map(fn => fn({}))
+  },
   setState(newValue) {
     store.state = newValue
+    store.update()
   }
 }
 const reducer = (state, action) => {
@@ -35,18 +47,18 @@ export const App = () => {
 const Child1 = () => <section>Child1<User/></section>
 const Child2 = () => <section>Child2<UserModifier/></section>
 const Child3 = () => <section>Child3</section>
-const User = () => {
-  const {state} = useContext(appContext)
-  return <div>User:{state.user.name}</div>
 
-}
-
+//所有用到store数据的组件都应该用connect包裹
 const connect = (Component) => {
   //返回一个对Component进行了包装的新函数组件
   return () => {
     //使用一个setState，在dispatch变更数据时，进行渲染
-    const [_, update] = useState({})
+    const [, update] = useState({})
     const {state, setState} = useContext(appContext)
+    //每个依赖state的组件订阅数据变动
+    useEffect(() => {
+      store.subscribe(update)
+    }, [])
     const dispatch = (action) => {
       setState(reducer(state, action))
       update({})
@@ -54,6 +66,10 @@ const connect = (Component) => {
     return <Component dispatch={dispatch} appState={state}/>
   }
 }
+const User = connect(() => {
+  const {state} = useContext(appContext)
+  return <div>User:{state.user.name}</div>
+})
 const UserModifier = connect((props) => {
   const {dispatch, appState} = props
   const onChange = (e) => {
