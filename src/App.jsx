ff19e4443f7da1e213bcfe4b0b236aec0d3fa39a
howardyangyixuan <howardyangyixuan@gmail.com>
2022-05-13 10:35:28 +0800

    为connect返回的组件添加props，并能够透传给被封装的组件

--- src/App.jsx
+++ src/App.jsx
@@ -45,13 +45,13 @@ export const App = () => {
   )
 }
 const Child1 = () => <section>Child1<User/></section>
-const Child2 = () => <section>Child2<UserModifier/></section>
+const Child2 = () => <section>Child2 <UserModifier>Other Props</UserModifier></section>
 const Child3 = () => <section>Child3</section>
 
 //所有用到store数据的组件都应该用connect包裹
 const connect = (Component) => {
   //返回一个对Component进行了包装的新函数组件
-  return () => {
+  return (props) => {
     //使用一个setState，在dispatch变更数据时，进行渲染
     const [, update] = useState({})
     const {state, setState} = useContext(appContext)
@@ -63,7 +63,7 @@ const connect = (Component) => {
       setState(reducer(state, action))
       update({})
     }
-    return <Component dispatch={dispatch} appState={state}/>
+    return <Component {...props} dispatch={dispatch} appState={state}/>
   }
 }
 const User = connect(() => {
@@ -71,12 +71,13 @@ const User = connect(() => {
   return <div>User:{state.user.name}</div>
 })
 const UserModifier = connect((props) => {
-  const {dispatch, appState} = props
+  const {dispatch, appState, children} = props
   const onChange = (e) => {
     dispatch({type: "updateUser", payload: {name: e.target.value}})
   }
   return <div>
     <input value={appState.user.name}
            onChange={onChange}/>
+    {children}
   </div>
 })
