185e4b8beba4ea471c7f7f9f9fbab6e47f48be71
howardyangyixuan <howardyangyixuan@gmail.com>
2022-05-13 11:02:44 +0800

    封装极简版redux:并导出context/connect/state

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,6 @@
-import React, {useState, useContext, useEffect} from "react"
+import React, {useContext} from "react"
+import {appContext, connect, store} from "./redux"
 
-const appContext = React.createContext(null)
-const store = {
-  state: {
-    user: {name: "howard", age: 23}
-  },
-  listeners: [],
-  subscribe(fn) {
-    store.listeners.push(fn)
-    return () => {
-      const idx = store.listeners.indexOf(fn)
-      this.listeners.splice(idx, 1)
-    }
-  },
-  update() {
-    store.listeners.map(fn => fn({}))
-  },
-  setState(newValue) {
-    store.state = newValue
-    store.update()
-  }
-}
-const reducer = (state, action) => {
-  const {type, payload} = action
-  if (type === "updateUser") {
-    return {
-      ...state,
-      user: {
-        ...state.user,
-        ...payload
-      }
-    }
-  } else {
-    return state
-  }
-}
 export const App = () => {
   return (
     <appContext.Provider value={store}>
@@ -48,24 +14,6 @@ const Child1 = () => <section>Child1<User/></section>
 const Child2 = () => <section>Child2 <UserModifier>Other Props</UserModifier></section>
 const Child3 = () => <section>Child3</section>
 
-//所有用到store数据的组件都应该用connect包裹
-const connect = (Component) => {
-  //返回一个对Component进行了包装的新函数组件
-  return (props) => {
-    //使用一个setState，在dispatch变更数据时，进行渲染
-    const [, update] = useState({})
-    const {state, setState} = useContext(appContext)
-    //每个依赖state的组件订阅数据变动
-    useEffect(() => {
-      store.subscribe(update)
-    }, [])
-    const dispatch = (action) => {
-      setState(reducer(state, action))
-      update({})
-    }
-    return <Component {...props} dispatch={dispatch} appState={state}/>
-  }
-}
 const User = connect(() => {
   const {state} = useContext(appContext)
   return <div>User:{state.user.name}</div>
---
+++ src/redux.jsx
@@ -0,0 +1,55 @@
+import React, {useContext, useEffect, useState} from "react"
+
+export const appContext = React.createContext(null)
+export const store = {
+  state: {
+    user: {name: "howard", age: 23}
+  },
+  listeners: [],
+  subscribe(fn) {
+    store.listeners.push(fn)
+    return () => {
+      const idx = store.listeners.indexOf(fn)
+      this.listeners.splice(idx, 1)
+    }
+  },
+  update() {
+    store.listeners.map(fn => fn({}))
+  },
+  setState(newValue) {
+    store.state = newValue
+    store.update()
+  }
+}
+const reducer = (state, action) => {
+  const {type, payload} = action
+  if (type === "updateUser") {
+    return {
+      ...state,
+      user: {
+        ...state.user,
+        ...payload
+      }
+    }
+  } else {
+    return state
+  }
+}
+//所有用到store数据的组件都应该用connect包裹
+export const connect = (Component) => {
+  //返回一个对Component进行了包装的新函数组件
+  return (props) => {
+    //使用一个setState，在dispatch变更数据时，进行渲染
+    const [, update] = useState({})
+    const {state, setState} = useContext(appContext)
+    //每个依赖state的组件订阅数据变动
+    useEffect(() => {
+      store.subscribe(update)
+    }, [])
+    const dispatch = (action) => {
+      setState(reducer(state, action))
+      update({})
+    }
+    return <Component {...props} dispatch={dispatch} appState={state}/>
+  }
+}
