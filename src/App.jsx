58f6e88c79b0e67d92652416678c6b3bddfa8f7b
howardyangyixuan <howardyangyixuan@gmail.com>
2022-05-13 09:12:25 +0800

    首先将app中使用的useState换为全局变量store

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import React, {useState, useContext} from "react"
 
 const appContext = React.createContext(null)
+const store = {
+  state: {
+    user: {name: "howard", age: 23}
+  },
+  setState(newValue) {
+    store.state = newValue
+  }
+}
 const reducer = (state, action) => {
   const {type, payload} = action
   if (type === "updateUser") {
@@ -16,12 +24,8 @@ const reducer = (state, action) => {
   }
 }
 export const App = () => {
-  const [appState, setAppState] = useState({
-    user: {name: "howard", age: 23}
-  })
-  const contextValue = {appState, setAppState}
   return (
-    <appContext.Provider value={contextValue}>
+    <appContext.Provider value={store}>
       <Child1/>
       <Child2/>
       <Child3/>
@@ -32,19 +36,22 @@ const Child1 = () => <section>Child1<User/></section>
 const Child2 = () => <section>Child2<UserModifier/></section>
 const Child3 = () => <section>Child3</section>
 const User = () => {
-  const {appState} = useContext(appContext)
-  return <div>User:{appState.user.name}</div>
+  const {state} = useContext(appContext)
+  return <div>User:{state.user.name}</div>
 
 }
 
 const connect = (Component) => {
   //返回一个对Component进行了包装的新函数组件
   return () => {
-    const {appState, setAppState} = useContext(appContext)
+    //使用一个setState，在dispatch变更数据时，进行渲染
+    const [_, update] = useState({})
+    const {state, setState} = useContext(appContext)
     const dispatch = (action) => {
-      setAppState(reducer(appState, action))
+      setState(reducer(state, action))
+      update({})
     }
-    return <Component dispatch={dispatch} appState={appState}/>
+    return <Component dispatch={dispatch} appState={state}/>
   }
 }
 const UserModifier = connect((props) => {
